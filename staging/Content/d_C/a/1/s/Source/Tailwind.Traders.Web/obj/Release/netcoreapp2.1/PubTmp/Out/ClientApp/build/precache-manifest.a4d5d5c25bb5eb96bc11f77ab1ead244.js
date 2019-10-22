9f79b3891ee4055cf565913dc3fe42341adb2b5d
abel <abel.wang@gmail.com>
2019-10-22 02:01:49 -0700

    added rest of staging

---
+++ staging/Content/d_C/a/1/s/Source/Tailwind.Traders.Web/obj/Release/netcoreapp2.1/PubTmp/Out/ClientApp/build/precache-manifest.a4d5d5c25bb5eb96bc11f77ab1ead244.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa5d65f2f0230b5917fe3331684a1e11",
+    "url": "/static/media/profile-plant.fa5d65f2.jpg"
+  },
+  {
+    "revision": "6fb84a93c95113fc3ef0",
+    "url": "/static/css/main.1a9d39e1.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "de733a933e0a4224577901c30b4a08ac",
+    "url": "/static/media/icon-filter.de733a93.svg"
+  },
+  {
+    "revision": "44423fbe432311a98482",
+    "url": "/static/js/2.081830ec.chunk.js"
+  },
+  {
+    "revision": "dc4cea67d0c1c7ce36fb3577aaa34e4d",
+    "url": "/static/media/home_plumbing.dc4cea67.jpg"
+  },
+  {
+    "revision": "ef4fe303957016ec6e082539b60e7db4",
+    "url": "/static/media/home_gardencenter.ef4fe303.jpg"
+  },
+  {
+    "revision": "ac4e652e9adc401cac640b6cf9d5549b",
+    "url": "/static/media/home_electrical.ac4e652e.jpg"
+  },
+  {
+    "revision": "9b6d63ad114a06d0dff2866858c54043",
+    "url": "/static/media/Mockup_Small.9b6d63ad.png"
+  },
+  {
+    "revision": "6d0b291b42927c1854d358401ed89178",
+    "url": "/static/media/Mockup_Small@2x.6d0b291b.png"
+  },
+  {
+    "revision": "8c0557e0a785aef80aef06a706171343",
+    "url": "/static/media/herobg.8c0557e0.jpg"
+  },
+  {
+    "revision": "dac693ccc98fd5b8c1531d0e985c46ec",
+    "url": "/static/media/Mockup_Big.dac693cc.png"
+  },
+  {
+    "revision": "d20e31912a4c6703ae1212f393b34638",
+    "url": "/static/media/Mockup_Big@2x.d20e3191.png"
+  },
+  {
+    "revision": "b8b833c1e5267921ea06f0ef1a6d4924",
+    "url": "/static/media/home_powertools.b8b833c1.jpg"
+  },
+  {
+    "revision": "6fb84a93c95113fc3ef0",
+    "url": "/static/js/main.ecf1f2ad.chunk.js"
+  },
+  {
+    "revision": "4aea2c020d9aadb6b20251026dd0e63e",
+    "url": "/static/media/profile-sink.4aea2c02.jpg"
+  },
+  {
+    "revision": "b5f532a4c7439f9ad47a0716d4596e74",
+    "url": "/static/media/profile-kitchen.b5f532a4.jpg"
+  },
+  {
+    "revision": "bdb965c58646c78bcd0f483974917b4f",
+    "url": "/static/media/icon-rim.bdb965c5.svg"
+  },
+  {
+    "revision": "aac11708e74232bb73bc9d68d1ade6c0",
+    "url": "/static/media/logo-horizontal.aac11708.svg"
+  },
+  {
+    "revision": "834282f2e3b3bc756958d8d2d2e43a99",
+    "url": "/static/media/icon-close.834282f2.svg"
+  },
+  {
+    "revision": "a73cc7417630202af78ba5f1e9f9cd03",
+    "url": "/static/media/icon-profile.a73cc741.svg"
+  },
+  {
+    "revision": "716671928718781c625111d361c2d0f2",
+    "url": "/static/media/icon-menu.71667192.svg"
+  },
+  {
+    "revision": "f5740803f46e242bc86e285cfa99cb6f",
+    "url": "/static/media/icon-cart.f5740803.svg"
+  },
+  {
+    "revision": "f3139ea90c7ef5d81b628cf79b0745d0",
+    "url": "/static/media/icon-photo.f3139ea9.svg"
+  },
+  {
+    "revision": "3be4bc2bc5d32a26249d219f5fbeb9b4",
+    "url": "/static/media/icon-freeshipping.3be4bc2b.svg"
+  },
+  {
+    "revision": "aa37a37fe0241427a15601ffe16c16be",
+    "url": "/static/media/icon-coupons.aa37a37f.svg"
+  },
+  {
+    "revision": "44423fbe432311a98482",
+    "url": "/static/css/2.60508ab6.chunk.css"
+  },
+  {
+    "revision": "1a4b32e682be86664ad836c9e93c9d13",
+    "url": "/index.html"
+  }
+];
